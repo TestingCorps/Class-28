47cf5e4f690512a9215ed12fffb3e9744f7f802c
Tiffany Jiang <ying.tiffany2006@gmail.com>
2020-12-30 20:51:40 -0500

    contraints

---
+++ chain.js
@@ -0,0 +1,22 @@
+class Chain{
+    constructor(bodyA,bodyB){
+        var options ={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.04,
+            length:10
+        }
+
+        this.chain = Constraint.create(options);
+        World.add(world,this.chain);
+    }
+    
+    display(){
+        var PA = this.chain.bodyA.position;
+        var PB = this.chain.bodyB.position;
+        strokeWeight(5);
+        line(PA.x,PA.y,PB.x,PB.y);
+
+    }
+};
+  
